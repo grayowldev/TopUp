a02a837f18d72eea1e5d7d043d6e80b7bb6d0108
grayowldev <truegrayowl@gmail.com>
2017-06-30 12:50:19 -0400

    home updates

--- app/src/main/java/strada/io/topup/HomeActivity.java
+++ app/src/main/java/strada/io/topup/HomeActivity.java
@@ -1,5 +1,10 @@
 package strada.io.topup;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,8 +23,27 @@ public class HomeActivity extends AppCompatActivity {
         checkBalBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });
+    }
+
+    public void startCallFunction(String functionCode) {
+        if (!functionCode.equals(null)) {
+            Uri functionUri = Uri.parse(functionCode);
+            Intent functionIntent = new Intent(Intent.ACTION_CALL, functionUri);
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            startActivity(functionIntent);
+        }
     }
 }
